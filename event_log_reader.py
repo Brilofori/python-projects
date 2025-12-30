54e252a22b34bc4e9e4f342ed5a4408f7a1bbe1e
Bril Adu <bril.ofori@gmail.com>
2025-12-30 19:17:12 +1100

    fixed some incorrect conditionals that were affecting the functionality of the code at run time

--- event_log_reader.py
+++ event_log_reader.py
@@ -15,33 +15,34 @@ import win32evtlog
 import win32evtlogutil
 import datetime 
 
-
 def get_security_logs():
     server = 'localhost'
     logtype = 'Security'
     hand = win32evtlog.OpenEventLog(server, logtype)
+
     if logtype:
         print(f'sucessfully retreived {logtype} information')
-    else:
-        print(f'error with {logtype} format retreival')
     return hand 
+
 hand = get_security_logs()
 
 def display_event_metadata(event):
-    print(f'Event ID: {event.EventID}')
+    event_id = event.EventID & 0xFFFF
+    time = event.TimeGenerated.Format()
+
+    print(f'Event ID: {event_id}')
     print(f'Source: {event.SourceName}')
-    print(f'Time Generated: {event.TimeGenerated}')
+    print(f'Time Generated: {time}')
     print('-' * 40)
 
 
 def read_security_log_events():
-    logs = win32evtlog.GetNumberOfEventLogRecords(hand)
+
     flags = win32evtlog.EVENTLOG_BACKWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ
-    offset = 0 
-    
+
     try:
         while True:
-            events = win32evtlog.ReadEventLog(hand, flags, offset)
+            events = win32evtlog.ReadEventLog(hand, flags, 0)
             if not events:
                 break
         
@@ -51,7 +52,5 @@ def read_security_log_events():
     except Exception as e:
         print(f'error occured at: {e}')
     
-    print(f"Total records in Security log: {logs}")
-
 read_security_log_events()
 
