8e8e65cb8f95e5ba8fbea5b10559548f78e1abe8
Bril Adu <bril.ofori@gmail.com>
2026-01-03 11:04:57 +1100

    successfully retrived ip rhost values

--- linux-auth-failure-analyzer.py
+++ linux-auth-failure-analyzer.py
@@ -1,2 +1,23 @@
-with('Linux_2k.log') as linuxLogs:
-    print(linuxLogs)
+logFile = 'Logs/Linux_2k.log'
+count = 0
+with open(logFile, 'r') as logs:
+    for log in logs:
+        if 'authentication failure' in log.lower():
+            parts = log.split()
+            
+            
+
+            for rhostValue in parts:
+                if rhostValue.startswith("rhost="):
+                    ipaddr = rhostValue.split('=')[1]
+                    for x in ipaddr:
+                        count += 1
+                        print({ipaddr:count})
+                    
+ 
+                    
+                        
+                
+
+
+
